30e968f21ef98bff4e6316459ba8aa98f28b4b3e
Sotiris Falieris <sotiris.falieris@electrolux.com>
2025-09-15 09:48:17 +0200

    Set gradle scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,29 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+apply { plugin("com.github.ben-manes.versions") }
+
+//Check versions of dependencies: ./gradlew dependencyUpdates -Drevision=milestone -DoutputFormatter=json
+//Force-update dependencies:      ./gradlew clean build --refresh-dependencies
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.versionsCheck) apply false
+}
+
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+// Reject all non-stable versions
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
