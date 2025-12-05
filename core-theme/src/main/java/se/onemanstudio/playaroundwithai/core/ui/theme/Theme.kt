274ac1ec23021ef1741402e5965dd0d7d5378d89
Sotiris Falieris <sotiris.falieris@electrolux.com>
2025-12-05 15:53:31 +0100

    Rename the ui module to theme to better reflect its fuction

--- core-theme/src/main/java/se/onemanstudio/playaroundwithai/core/ui/theme/Theme.kt
+++ core-theme/src/main/java/se/onemanstudio/playaroundwithai/core/ui/theme/Theme.kt
@@ -47,7 +47,7 @@ fun SofaAiTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
