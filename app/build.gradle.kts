74434163652843c1e8447289f675bac239fa79be
Sotiris Falieris <sotiris@onemanstudio.se>
2025-12-15 15:52:49 +0100

    Trying to sort out CI secrets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.hilt)
     alias(libs.plugins.ksp)
-    alias(libs.plugins.maps.secrets)
 }
 
 // Read the API key from local.properties
