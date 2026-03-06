70f6c4e4e11771e548137b2fc219da975a1dee14
Sotiris Falieris <sotiris@onemanstudio.se>
2026-03-06 18:31:15 +0100

    Mark classes as immutable

--- feature/chat/src/main/java/se/onemanstudio/playaroundwithai/feature/chat/models/Attachment.kt
+++ feature/chat/src/main/java/se/onemanstudio/playaroundwithai/feature/chat/models/Attachment.kt
@@ -3,6 +3,7 @@ package se.onemanstudio.playaroundwithai.feature.chat.models
 import android.net.Uri
 import se.onemanstudio.playaroundwithai.data.chat.domain.model.AnalysisType
 
+@androidx.compose.runtime.Immutable
 sealed class Attachment {
     data class Image(val uri: Uri, val analysisType: AnalysisType) : Attachment()
     data class Document(val uri: Uri) : Attachment()
--- feature/chat/src/main/java/se/onemanstudio/playaroundwithai/feature/chat/models/SnackbarEvent.kt
+++ feature/chat/src/main/java/se/onemanstudio/playaroundwithai/feature/chat/models/SnackbarEvent.kt
@@ -1,5 +1,6 @@
 package se.onemanstudio.playaroundwithai.feature.chat.models
 
+@androidx.compose.runtime.Immutable
 sealed interface SnackbarEvent {
     data object LocalSaveFailed : SnackbarEvent
     data object LocalUpdateFailed : SnackbarEvent
